259ef7f5f6bc24052772ce824b9023d535777864
Jadon Duby <jadon.duby@neofinancial.com>
2025-12-24 10:23:19 -0700

    adds unified metrics logging

--- callbacks/callback.py
+++ callbacks/callback.py
@@ -22,6 +22,7 @@ class EvaluationCallback(Callback):
     def on_episode_end(self, ctx, interval = 50, eval_episodes = 3):
         ep=ctx.episode
         if ep % interval == 0:
+            # evaluator = PolicyEvaluator(env=self.env, agent=self.agent, record_dir=f"eval/policy_{self.policy}/inter_train_eval/")
             evaluator = PolicyEvaluator(env=self.env, agent=self.agent, record_dir=f"eval/policy_{self.policy}/inter_train_eval/")
             truncated, frames, env = evaluator.evaluate(num_eval_episodes=eval_episodes, file_name=f"ep_{ep}_eval.mp4")
             # return truncated, frames, env
@@ -122,7 +123,7 @@ class PlotRewardsCallback(Callback):
         plt.ylabel('reward')
         window_size = 10
         rewards = ctx.rewards
-        print(f"{rewards=}")
+        print(f"info passed to rewards callback on episode end: {ctx.info}")
         if len(rewards) > window_size:
             # print(f"{len(durations_t)=}")
             weights = np.ones(window_size) / window_size
@@ -131,3 +132,96 @@ class PlotRewardsCallback(Callback):
             plt.plot(rewards)
             plt.pause(0.001)  # pause a bit so that plots are updated
             plt.savefig("eval/reward.png")
+
+class MetricsCallback:
+    def __init__(self):
+        self.episode_durations = []
+        self.episode_rewards = []
+        self.losses = []
+        
+        # running trackers for the current episode:
+        self._ep_reward = 0
+        self._ep_len = 0
+        self._ep_epsilon = []
+
+    def set_trainer(self, trainer):
+        self.trainer = trainer
+
+    def on_train_begin(self, logs ):
+        return 
+
+    def on_step_end(self, ctx):
+        self._ep_reward += ctx.reward
+        self._ep_len += 1
+        self._ep_epsilon.append(ctx.epsilon)
+        loss = ctx.extra["loss"]
+        self.losses.append(loss.cpu().item()) if loss else None
+
+    def on_episode_end(self, ctx):
+        self.episode_durations.append(self._ep_len)
+        self.episode_rewards.append(self._ep_reward)
+
+        # Attach metrics back into callback context
+        # ctx.extra["episode_durations"] = self._ep_len
+        # ctx.extra["episode_reward"] = self._ep_reward
+
+        self._plot_durations(self.episode_durations)
+        self._plot_rewards(self.episode_rewards)
+        self._plot_epsilon(self._ep_epsilon)
+        self._plot_losses(self.losses)
+
+        self._ep_len = 0
+        self._ep_reward = 0
+
+        return {"episode_reward": self._ep_reward, "episode_length": self._ep_len}
+    
+    def _plot_rewards(self, rewards):
+        
+        plt.figure(1)
+        plt.clf()
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('reward')
+        window_size = 10
+        if len(rewards) > window_size:
+            # print(f"{len(durations_t)=}")
+            weights = np.ones(window_size) / window_size
+            sma = np.convolve(rewards, weights, mode='same')
+            # plt.plot(range(0,len(rewards)), sma) # test metric is number of frames the episode lasted. 500 is success
+            plt.plot(rewards)
+            plt.pause(0.001)  # pause a bit so that plots are updated
+            plt.savefig("eval/reward.png")
+
+    def _plot_epsilon(self, epsilons):
+        plt.figure(2)
+        plt.clf()
+        plt.plot(epsilons)
+        plt.title("Epsilon Decay")
+        plt.xlabel("Step")
+        plt.ylabel("Epsilon")
+        plt.savefig(f"eval/epsilon_curve.jpg")
+
+    def _plot_losses(self, losses):
+        plt.figure(3)
+        plt.clf()
+        plt.plot(losses)
+        plt.title("Loss")
+        plt.xlabel("Step")
+        plt.ylabel("Epsilon")
+        plt.savefig(f"eval/loss_curve.jpg")
+
+    def _plot_durations(self, episode_durations):        
+        plt.figure(1)
+        plt.clf()
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        window_size = 10
+        if len(episode_durations) > window_size:
+            # print(f"{len(durations_t)=}")
+            weights = np.ones(window_size) / window_size
+            sma = np.convolve(episode_durations, weights, mode='same')
+            plt.plot(range(0,len(episode_durations)), sma) # test metric is number of frames the episode lasted. 500 is success
+            plt.plot(episode_durations)
+            plt.pause(0.001)  # pause a bit so that plots are updated
+            plt.savefig("eval/duration.png")
